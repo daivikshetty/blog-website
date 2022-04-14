59cf9c91e7b4d491a7895d5a9d81895d9cdfa385
Daivik Shetty <shettydaivik100@gmail.com>
2022-04-14 13:32:26 +0530

    Added css and links

--- app.js
+++ app.js
@@ -9,13 +9,52 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-
+const server=3000;        //runs on server 3000 only
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get("/",function(req,res){
+  res.render("home",{
+    postContent:homeStartingContent
+  })
+});
+
+app.post("/",function(req,res){
+  
+});
+
+app.get("/about",function(req,res){
+  res.render("about",{
+      about:aboutContent
+  })
+})
+
+app.post("/about",function(req,res){
+
+})
+
+app.get("/contact",function(req,res){
+  res.render("contact",{
+      contact:contactContent
+  })
+})
+
+app.post("/contact",function(req,res){
+  
+})
+
+app.get("/compose",function(req,res){
+  res.render("compose",{
+  })
+})
+
+app.post("/compose",function(req,res){
+  console.log(req.body.composeTitle);
+  console.log(req.body.composeAddContent);
+})
 
 
 
@@ -23,10 +62,6 @@ app.use(express.static("public"));
 
 
 
-
-
-
-
-app.listen(3000, function() {
+app.listen(server, function() {
   console.log("Server started on port 3000");
 });
